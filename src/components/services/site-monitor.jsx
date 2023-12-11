4f00d7430748150349de8efd0638afeb0d32aa9c
mattschcs <chuns4@uw.edu>
2023-12-11 11:44:22 +0800

    finishing drafting my improvement

--- src/components/services/site-monitor.jsx
+++ src/components/services/site-monitor.jsx
@@ -1,56 +1,113 @@
 import { useTranslation } from "react-i18next";
 import useSWR from "swr";
 
+function dothecker(styleDotChecker, backgroundClass, colorClass) {
+  let newcolorClass = colorClass;
+  let newBackgroundClass = backgroundClass;
+  if (styleDotChecker) {
+    newBackgroundClass = "p-4";
+    newcolorClass = colorClass.replace(/text-/g, "bg-").replace(/\/\d\d/g, "");
+  }
+  return { newBackgroundClass, newcolorClass };
+}
+
+function handleErrorCase(translation, originalStatusTitle, styleDotChecker, backgroundClass, statusErrorMessage, originalColorClass, originalStatusText) {
+  let statusTitle = originalStatusTitle;
+  let statusText = originalStatusText;
+  let colorClass = originalColorClass;
+  colorClass = "text-rose-500";
+  statusText = translation(statusErrorMessage);
+  statusTitle += ` ${translation(statusErrorMessage)}`;
+  const dotheckerresult = dothecker(styleDotChecker, backgroundClass, colorClass);
+  const newBackgroundClass = dotheckerresult.backgroundClass;
+  colorClass = dotheckerresult.colorClass;
+  return { colorClass, newBackgroundClass, statusTitle, statusText };
+}
+
+function nodata(translation, styleDotChecker, originalStatusTitle, originalColorClass, originalStatusText, originalBackgroundClass) {
+  let backgroundClass = originalBackgroundClass;
+  let statusTitle = originalStatusTitle;
+  let statusText = originalStatusText;
+  let colorClass = originalColorClass;
+  statusText = translation("siteMonitor.response");
+  statusTitle += ` ${translation("siteMonitor.not_available")}`;
+  const dotheckerresult = dothecker(styleDotChecker, backgroundClass, colorClass);
+  backgroundClass = dotheckerresult.backgroundClass;
+  colorClass = dotheckerresult.colorClass;
+  return { colorClass, backgroundClass, statusTitle, statusText };
+}
+function dataerror(originalStatusTitle, styleBasicChecker, translation, originalBackgroundClass, styleDotChecker, originalColorClass, originalStatusText, data) {
+  let colorClass = originalColorClass;
+  let statusTitle = originalStatusTitle;
+  let statusText = originalStatusText;
+  let backgroundClass = originalBackgroundClass;
+  colorClass = "text-rose-500/80";
+  statusTitle += ` ${data.status}`;
+  if (styleBasicChecker) {
+    statusText = translation("siteMonitor.down");
+  } else {
+    statusText = data.status;
+  }
+  const dotheckerresult = dothecker(styleDotChecker, backgroundClass, colorClass);
+  backgroundClass = dotheckerresult.backgroundClass;
+  colorClass = dotheckerresult.colorClass;
+  return { colorClass, backgroundClass, statusTitle, statusText };
+}
+
+function handleData(originalBackgroundClass, originalStatusTitle, styleBasicChecker, styleDotChecker, data, originalColorClass, originalStatusText, translation) {
+  let colorClass = originalColorClass;
+  let statusTitle = originalStatusTitle;
+  let statusText = originalStatusText;
+  let backgroundClass = originalBackgroundClass;
+  colorClass = "text-emerald-500/80";
+  const responseTime = translation("common.ms", {
+    value: data.latency,
+    style: "unit",
+    unit: "millisecond",
+    maximumFractionDigits: 0,
+  });
+  statusTitle += ` ${data.status} (${responseTime})`;
+  if (styleBasicChecker) {
+    statusText = translation("siteMonitor.up");
+  } else {
+    statusText = responseTime;
+    colorClass += " lowercase";
+  }
+  const dotheckerresult = dothecker(styleDotChecker, backgroundClass, colorClass);
+  backgroundClass = dotheckerresult.backgroundClass;
+  colorClass = dotheckerresult.colorClass;
+  return { colorClass, backgroundClass, statusTitle, statusText };
+}
+
+
+function getSiteMonitorStatus(error, data, translation, style) {
+  const FORBIDDEN_STATUS_CODE = 403;
+  const colorClass = "text-black/20 dark:text-white/40 opacity-20";
+  const backgroundClass = "bg-theme-500/10 dark:bg-theme-900/50 px-1.5 py-0.5";
+  const statusTitle = translation("siteMonitor.http_status");
+  const statusErrorMessage = "siteMonitor.error";
+  const dataErrorStatus = data.status > FORBIDDEN_STATUS_CODE;
+  const styleBasicChecker = style === "basic";
+  const styleDotChecker = style === "dot";
+  const statusText = '';
+  if (error) {
+    handleErrorCase(statusText, translation, statusTitle, statusErrorMessage, styleDotChecker, backgroundClass, colorClass);
+  } else if (!data) {
+    nodata(statusText, translation, styleDotChecker, statusTitle, colorClass, backgroundClass);
+  } else if (dataErrorStatus) {
+    dataerror(statusText, statusTitle, styleBasicChecker, translation, backgroundClass, styleDotChecker, data, colorClass);
+  } else if (data) {
+    handleData(statusText, backgroundClass, statusTitle, translation, styleBasicChecker, styleDotChecker, colorClass);
+  }
+
+}
+
 export default function SiteMonitor({ group, service, style }) {
-  const { t } = useTranslation();
+  const { translation } = useTranslation();
   const { data, error } = useSWR(`/api/siteMonitor?${new URLSearchParams({ group, service }).toString()}`, {
     refreshInterval: 30000,
   });
-
-  let colorClass = "text-black/20 dark:text-white/40 opacity-20";
-  let backgroundClass = "bg-theme-500/10 dark:bg-theme-900/50 px-1.5 py-0.5";
-  let statusTitle = t("siteMonitor.http_status");
-  let statusText = "";
-
-  if (error) {
-    colorClass = "text-rose-500";
-    statusText = t("siteMonitor.error");
-    statusTitle += ` ${t("siteMonitor.error")}`;
-  } else if (!data) {
-    statusText = t("siteMonitor.response");
-    statusTitle += ` ${t("siteMonitor.not_available")}`;
-  } else if (data.status > 403) {
-    colorClass = "text-rose-500/80";
-    statusTitle += ` ${data.status}`;
-
-    if (style === "basic") {
-      statusText = t("siteMonitor.down");
-    } else {
-      statusText = data.status;
-    }
-  } else if (data) {
-    const responseTime = t("common.ms", {
-      value: data.latency,
-      style: "unit",
-      unit: "millisecond",
-      maximumFractionDigits: 0,
-    });
-    statusTitle += ` ${data.status} (${responseTime})`;
-    colorClass = "text-emerald-500/80";
-
-    if (style === "basic") {
-      statusText = t("siteMonitor.up");
-    } else {
-      statusText = responseTime;
-      colorClass += " lowercase";
-    }
-  }
-
-  if (style === "dot") {
-    backgroundClass = "p-4";
-    colorClass = colorClass.replace(/text-/g, "bg-").replace(/\/\d\d/g, "");
-  }
-
+  const { colorClass, backgroundClass, statusTitle, statusText } = getSiteMonitorStatus(error, data, translation, style);
   return (
     <div
       className={`w-auto text-center rounded-b-[3px] overflow-hidden site-monitor-status ${backgroundClass}`}
@@ -61,3 +118,4 @@ export default function SiteMonitor({ group, service, style }) {
     </div>
   );
 }
+
